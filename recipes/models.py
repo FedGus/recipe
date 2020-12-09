a215cc0e9662ac2109ea1ab1493e9090d8689479
Fedor Gusev <gusevfedya@mail.ru>
2020-12-09 13:41:28 +0300

    creating database models

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.conf import settings
 
 class Ingredients(models.Model):
-
     ingredient_name = models.CharField("Ингредиент", max_length=150)
     proteins = models.PositiveSmallIntegerField("Белки", default=0)
     fats = models.PositiveSmallIntegerField("Жиры", default=0)
@@ -13,3 +13,84 @@ class Ingredients(models.Model):
     class Meta:
         verbose_name = "Ингредиент"
         verbose_name_plural = "Ингредиенты"
+
+class Country(models.Model):
+    country_name = models.CharField("Страна", max_length=150)
+
+    def __str__(self):
+        return self.country_name
+
+    class Meta:
+        verbose_name = "Страна"
+        verbose_name_plural = "Страны"
+
+class Section(models.Model):
+    name_section = models.CharField("Название раздела", max_length=150)
+    description = models.TextField("Описание")
+
+    def __str__(self):
+        return self.name_section
+
+    class Meta:
+        verbose_name = "Раздел"
+        verbose_name_plural = "Разделы"
+
+class Kitchen(models.Model):
+    id_country = models.ForeignKey(Country, verbose_name="страны", on_delete=models.SET_NULL, null=True)
+    name_kitchen = models.CharField("Название кухни", max_length=150)
+
+    def __str__(self):
+        return self.name_kitchen
+
+    class Meta:
+        verbose_name = "Кухня"
+        verbose_name_plural = "Кухни"
+
+class Recipe(models.Model): 
+    id_section = models.ForeignKey(Section, verbose_name="раздел", on_delete=models.SET_NULL, null=True)
+    id_kitchen = models.ForeignKey(Kitchen, verbose_name="кухня", on_delete=models.SET_NULL, null=True)
+    ingredients = models.ManyToManyField(Ingredients, verbose_name="ингредиенты", related_name="ingredients_recipe")
+    title = models.CharField("Название рецепта", max_length=150)
+    description = models.TextField("Описание")
+    steps = models.TextField("Шаги приготовления")
+
+    id_user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="пользователь автор", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Рецепт"
+        verbose_name_plural = "Рецепты"
+
+class Favorites(models.Model):
+    id_recipe = models.ForeignKey(Recipe, verbose_name="рецепт", on_delete=models.SET_NULL, null=True)
+
+    id_user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="пользователь избранное", on_delete=models.SET_NULL, null=True)
+  
+    class Meta:
+        verbose_name = "Избранное"
+        verbose_name_plural = "Избранные рецепты"
+
+class Comments(models.Model):
+    id_recipe = models.ForeignKey(Recipe, verbose_name="рецепт", on_delete=models.SET_NULL, null=True)
+    comment = models.TextField("Комментарий")
+
+    id_user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="пользователь комментарий", on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.comment
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Коментарии"
+
+class Assessment(models.Model):
+    id_recipe = models.ForeignKey(Recipe, verbose_name="рецепт", on_delete=models.SET_NULL, null=True)
+    assessment = models.PositiveSmallIntegerField("Оценка", default=0)
+
+    id_user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="пользователь оценка", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Оценка"
+        verbose_name_plural = "Оценки"
