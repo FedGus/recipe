28741ef114699c956852c327bff2acaa71983583
Fedor <gusevfedya@mail.ru>
2021-01-11 18:21:59 +0300

    displaying images in the fields of models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
+from django.utils.safestring import mark_safe
 from .models import Ingredients, Country, Section, Kitchen, Recipe, Favorites, Comments, Assessment
 
 @admin.register(Ingredients)
@@ -17,7 +18,12 @@ admin.site.register(Kitchen)
 
 @admin.register(Recipe)
 class PracticeAdmin(ImportExportModelAdmin):
-    list_display = ("id_section", "id_kitchen", "title", "description", "steps", "image")
+    list_display = ("id_section", "id_kitchen", "title", "description", "steps", "get_image")
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="120"')
+
+    get_image.short_description = "Изображение"
     pass
 
 admin.site.register(Favorites)
